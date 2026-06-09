145489706ab778aacf9d7cdbfb8ac87a4b43bed0
Julen Recio <julenreciotocado@gmail.com>
2026-06-09 17:41:09 +0200

    Implemented ft_swap in two different ways

--- moves.c
+++ moves.c
@@ -5,15 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrecio-t <jrecio-t@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/09 09:26:45 by azahino-          #+#    #+#             */
-/*   Updated: 2026/06/09 13:21:33 by jrecio-t         ###   ########.fr       */
+/*   Created: 2026/06/09 17:22:31 by jrecio-t          #+#    #+#             */
+/*   Updated: 2026/06/09 17:38:11 by jrecio-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_sa(t_cll **list, int bench)
+t_cll	*ft_swap(t_cll **list)
 {
-	if (bench == 1)
-		ft_printf("sa");
+	t_cll	*node1;
+	t_cll	*node2;
+	t_cll	*node3;
+
+	if (*list == NULL || (*list)->next == NULL)
+		return (*list);
+	node1 = *list;
+	node2 = node1->next;
+	node3 = node2->next;
+	node2->next = node1;
+	node1->next = node3;
+	if (node3)
+		node3->prev = node1;
+	node1->prev = node2;
+	node2->prev = NULL;
+	return (node2);
 }
+/*
+Otra forma de hacerlo:
+
+t_cll	*ft_swap(t_cll *list)
+{
+	void	*temp;
+
+	if (list == NULL || list->next == NULL)
+		return (list);
+	temp = list->next->content;
+	list->next->content = list->content;
+	list->content = temp;
+	return (list);
+}
+*/

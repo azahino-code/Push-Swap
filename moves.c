4854c1dd45f72c3b987c4f2ca7036d1005662c08
Julen Recio <julenreciotocado@gmail.com>
2026-06-09 13:11:50 +0200

    Implemented new struct into disorder function

--- moves.c
+++ moves.c
@@ -3,30 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   moves.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azahino- <azahino-@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: jrecio-t <jrecio-t@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 09:26:45 by azahino-          #+#    #+#             */
-/*   Updated: 2026/06/09 10:06:00 by azahino-         ###   ########.fr       */
+/*   Updated: 2026/06/09 13:10:47 by jrecio-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    ft_sa(t_dll *a, int bench)
+void	ft_sa(t_cll **list, int bench)
 {
-    int cpy;
-    
-    if (bench > 0)
-        ft_printf("sa");
-}
-
-void    ft_sa(t_stack *a, int bench)
-{
-    int cpy;
-    
-    copy = a[0];
-    a[0] = a[1];
-    a[1] = copy;
-    if (bench > 0)
-        ft_printf("sa");
+	if (bench == 1)
+		ft_printf("sa");
 }
--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azahino- <azahino-@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: jrecio-t <jrecio-t@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 09:43:09 by azahino-          #+#    #+#             */
-/*   Updated: 2026/06/09 10:11:48 by azahino-         ###   ########.fr       */
+/*   Updated: 2026/06/09 13:01:26 by jrecio-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@
  #include <stdlib.h>
 
  typedef struct s_node
- {
-    int             value;
-    int             index;
-    struct s_node   *prev;
-    struct s_node   *next;
- }                  t_dll;
+{
+	void			*content;
+	struct s_node	*prev;
+	struct s_node	*next;
+}					t_cll;
 
 #endif

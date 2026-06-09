64b646f7fd5748986d3f352664e79529bbf9e2fb
Julen Recio <julenreciotocado@gmail.com>
2026-06-09 13:26:17 +0200

    Norminette corrections

--- main.c
+++ main.c
@@ -3,51 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azahino- <azahino-@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: jrecio-t <jrecio-t@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 13:45:49 by azahino-          #+#    #+#             */
-/*   Updated: 2026/06/09 10:11:35 by azahino-         ###   ########.fr       */
+/*   Updated: 2026/06/09 13:25:48 by jrecio-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char **argv)
-{
-    int option;
-    int bench;
-
-    bench = 0;    
-    
 // Trabajar las diferentes casuistuicas
-    option = getopt(argc, argv, "macsb:");
-    while (option != -1)
-    {
-        switch(option)
-        {
-            case 'b':
-                bench = 1;
-            case 'a':
-            {
-                ft_adaptive();
-            }
-            case 's':
-            {
-                ft_simple(argv);
-            }
-            case 'm':
-            {
-                ft_medium();
-            }
-            case 'c':
-            {
-                ft_complex();
-            }
-            case '?':
-              ft_printf("Unknown option:");
-            default:
-                ft_adaptive();
-        }
-    }
-    return (0);
+int	main(int argc, char **argv)
+{
+	int	option;
+	int	bench;
+
+	bench = 0;
+	option = getopt(argc, argv, "macsb:");
+	while (option != -1)
+	{
+		switch (option)
+		{
+			case 'b':
+				bench = 1;
+			case 'a':
+				ft_adaptive();
+			case 's':
+				ft_simple(argv);
+			case 'm':
+				ft_medium();
+			case 'c':
+				ft_complex();
+			case '?':
+				ft_printf("Unknown option:");
+			default :
+				ft_adaptive();
+		}
+	}
+	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,17 +6,17 @@
 /*   By: jrecio-t <jrecio-t@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 09:43:09 by azahino-          #+#    #+#             */
-/*   Updated: 2026/06/09 13:01:26 by jrecio-t         ###   ########.fr       */
+/*   Updated: 2026/06/09 13:22:43 by jrecio-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
- #define PUSH_SWAP_H
- 
- #include <unistd.h>
- #include <stdlib.h>
+# define PUSH_SWAP_H
 
- typedef struct s_node
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_node
 {
 	void			*content;
 	struct s_node	*prev;

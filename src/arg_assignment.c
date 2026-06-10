19ea17dfd6c5033302f071e7172c0d4cf3d347f4
azahino-code <aritzzahinosf@gmail.com>
2026-06-10 13:22:44 +0000

    Asignamiento de stack

--- src/arg_assignment.c
+++ src/arg_assignment.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   arg_assignment.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrecio-t <jrecio-t@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: azahino- <azahino-@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 13:18:59 by jrecio-t          #+#    #+#             */
-/*   Updated: 2026/06/10 13:50:13 by jrecio-t         ###   ########.fr       */
+/*   Updated: 2026/06/10 12:54:05 by azahino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,22 @@
 t_cll	*ft_assignment(int argc, char *argv, t_cll **stack_a)
 {
 	int		index;
+	char	**str;
 	int		value;
 	t_cll	*num;
 
 	index = 0;
 	while (index < argc)
-	{
-		value = ft_atoi(argv[index]); //12 13 54 26
+	{	
+		str = ft_split(argv[index], " ");
+		value = ft_atoi(str[index]); //12 13 54 26
 		if (value)
 		{
 			num = ft_lstnew(value);
 			ft_lstadd_back(*stack_a, num);
 		}
-		
+		free(str);
+		index++;
 	}
-	
+	return ()
 }

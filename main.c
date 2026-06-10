b345c310137a5324bbf8357e3ad913780be4b1bc
azahino-code <aritzzahinosf@gmail.com>
2026-06-10 08:22:22 +0000

    10-06-2026

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: azahino- <azahino-@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 13:45:49 by azahino-          #+#    #+#             */
-/*   Updated: 2026/06/09 10:11:35 by azahino-         ###   ########.fr       */
+/*   Updated: 2026/06/10 08:05:59 by azahino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,23 @@ int main(int argc, char **argv)
 {
     int option;
     int bench;
+    int i = 0;
 
-    bench = 0;    
+    bench = 0;
     
 // Trabajar las diferentes casuistuicas
-    option = getopt(argc, argv, "macsb:");
-    while (option != -1)
+    while (i++ < argc)
     {
-        switch(option)
-        {
-            case 'b':
-                bench = 1;
-            case 'a':
-            {
-                ft_adaptive();
-            }
-            case 's':
-            {
-                ft_simple(argv);
-            }
-            case 'm':
-            {
-                ft_medium();
-            }
-            case 'c':
-            {
-                ft_complex();
-            }
-            case '?':
-              ft_printf("Unknown option:");
-            default:
-                ft_adaptive();
-        }
+        if (ft_strcmp(*argv[i], "--simple"))
+            simple_al();
+        else if (*argv[i] == "--medium")
+            medium_al();
+        else if (*argv[i] == "--complex")
+            complex_al();
+        else if (*argv[i] == "--adaptative")
+            adaptative_al();
+        else if (*argv[i] == "" || *argv[i] == NULL)
+            adaptative_al();   
     }
     return (0);
 }

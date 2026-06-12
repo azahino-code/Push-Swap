10ddb910fdfa091bd004c7f4483ff4617d4d88a5
azahino-code <aritzzahinosf@gmail.com>
2026-06-12 10:50:19 +0000

    refactores disorder function

--- src/disorder.c
+++ src/disorder.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   disorder.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrecio-t <jrecio-t@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: azahino- <azahino-@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 10:41:11 by jrecio-t          #+#    #+#             */
-/*   Updated: 2026/06/10 11:21:16 by jrecio-t         ###   ########.fr       */
+/*   Updated: 2026/06/12 10:47:34 by azahino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 // Hay que comprobar que funciona
 
-float	compute_disorder(t_cll **a)
+float	compute_disorder(t_cll *a)
 {
 	int		mistakes;
 	int		total_pairs;
@@ -23,19 +23,21 @@ float	compute_disorder(t_cll **a)
 	void	*prev_content;
 
 	i = 0;
-	while (i < (int)sizeof(*a) - 1)
+	while (i < (int)sizeof(a->head) - 1)
 	{
 		j = i + 1;
-		while (j < (int)sizeof(*a) - 1)
+		while (j < (int)sizeof(a->head) - 1)
 		{
 			total_pairs += 1;
-			prev_content = (*a)->value;
-			*a = (*a)->next;
-			if (prev_content > (*a)->value)
+			prev_content = a->head->value;
+			a->head = a->head->next;
+			if (prev_content > a->head->value)
 			{
 				mistakes += 1;
 			}
+			j++;
 		}
+		i++;
 	}
 	return ((float)(mistakes / total_pairs));
 }

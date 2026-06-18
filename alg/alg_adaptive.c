43471d27a35bd11283072005777f8e4218e54673
azahino-code <aritzzahinosf@gmail.com>
2026-06-18 11:08:23 +0000

    alg_adaptive created

--- alg/alg_adaptive.c
+++ alg/alg_adaptive.c
@@ -6,6 +6,19 @@
 /*   By: azahino- <azahino-@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 10:04:20 by azahino-          #+#    #+#             */
-/*   Updated: 2026/06/11 10:04:21 by azahino-         ###   ########.fr       */
+/*   Updated: 2026/06/18 11:06:09 by azahino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../push_swap.h"
+
+t_cll	*alg_adaptive(float disorder, t_cll *stack_a, t_cll *stack_b)
+{
+	if (disorder < 0,2)
+		return (alg_selection(stack_a, stack_b));
+	else if (disorder >= 0,2 && disorder < 0,5)
+		return (alg_medium(stack_a, &stack_b, 
+			(int)sqrt(stack_a->size), stack_a->size));
+	else if (disorder <= 0,5)
+		return (alg_complex(stack_a, &stack_b));
+}
